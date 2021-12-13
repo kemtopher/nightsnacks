e4b00b30d4adffbeb3e389c650f608c28af856ce
Christopher <christopher.kemeza@gmail.com>
2021-12-12 19:58:21 -0500

    Sunday workday progress: page colorschemes, page title links, correct title sizing

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 import Highway from "@dogstudio/highway";
 import PageTransition from "./pages.js";
+import gsap from "gsap";
+import $ from 'jquery';
 
 const H = new Highway.Core({
   transitions: {
     default: PageTransition
   }
 });
+
+var timeout;
+$('body').mousemove(function(e){
+  if(timeout) clearTimeout(timeout);
+  setTimeout(callParallax.bind(null, e), 200);
+});
+
+function callParallax(e){
+  parallaxIt(e, '.images-well', -30);
+}
+
+function parallaxIt(e, target, movement){
+  var $this = $('body');
+  var relX = e.pageX - $this.offset().left;
+  var relY = e.pageY - $this.offset().top;
+  
+  gsap.to(target, 1, {
+    x: (relX - $this.width()/2) / $this.width() * movement,
+    y: (relY - $this.height()/2) / $this.height() * movement
+  })
+}
+
+
--- js/pages.js
+++ js/pages.js
@@ -1,68 +1,113 @@
 import Highway from "@dogstudio/highway";
 import gsap from "gsap";
-
+import $ from 'jquery';
 
 
 class PageTransition extends Highway.Transition {
     in({ from, to, done }) {
-        window.scrollTo(0, 0);
+        // window.scrollTo(0, 0);
         from.remove();
 
         function removeClass (targetClass) {
             const imageGroup = document.querySelectorAll('.images-well img');
-            
+
             imageGroup.forEach(el => {
-                console.log("Item: " + el.classList)
                 el.classList.remove(targetClass);
             })
         }
 
-        const tl = gsap.timeline();
+        const title = document.querySelector('.current-title');
+        title.addEventListener('click', (e) => {
+            // console.log("lello")
+            window.location.assign('localhost');
+        })
+
+        function getBgColor () {
+            const bgColor = to.dataset.page;
+            document.body.className = "";
+            document.body.classList.add("body-" + bgColor);
+        }
+        function followTheMouse () {
+            // const mouseFollow = document.querySelector('#mouse-follow');
+
+            // document.addEventListener('mousemove', (e) => {
+            // mouseFollow.style.cssText = `
+            //     left: ${e.clientX - 25}px;
+            //     top:  ${e.clientY - 25}px;
+            // `;
+            // });
+
+            var targets = document.querySelectorAll(".targetClass");
+
+            targets.forEach( x => {
+                function handlerIn (e) {
+                    const attribute = this.getAttribute('src');
+                    // document.querySelector('#mouse-follow').innerHTML = attribute;
+                    console.log(attribute);
+                }
+                function handlerOut () {
+                    // document.querySelector('#mouse-follow').innerHTML = '';
+                }
+                $( x ).mouseenter( handlerIn ).mouseleave( handlerOut );
+            })
+        }
+        // function dddd () {
+        //     var targets = document.querySelectorAll(".targetClass");
+
+        //     targets.forEach( x => {
+        //         // x.addEventListener("mouseover", (e) => {
+        //         //     // e.target.setAttribute("style", "opacity: 0.5;")
+        //         //     const attribute = e.target.getAttribute('src');
+        //         //     console.log(attribute);
+        //         //     // document.querySelector('#mouse-follow').innerHTML = attribute;
+        //         // });
+        //         function handlerIn () {
+        //             const attribute = this.getAttribute('src');
+        //             console.log(attribute);
+        //         }
+        //         function handlerOut () {
+        //             console.log("out");
+        //         }
+        //         $( x ).mouseenter( handlerIn ).mouseleave( handlerOut );
+        //     })
+        // }
+        
+        const tl = gsap.timeline({
+            onComplete: async () => {
+                await getBgColor();
+                await removeClass('inactive-card');
+                await followTheMouse();
+                await done();
+            }
+        });
+
         tl
-        .from(['.page-title-char'], {
+        .from('.page-title-char', {
             duration: 1,
-            yPercent: -100,
             ease: "power4",
+            yPercent: -100,
             stagger: 0.045
         },0.0)
-        .fromTo(['.bg-segment'], 0.5, { 
-            width: '0%',
-            ease: "power4"
+        .fromTo('.bg-segment', { 
+            width: '0%'
         },
         {
+            duration: 2.5,
             width: '100%',
-            stagger: 0.25,
-            onComplete: function() {
-                console.log("In")
-                const changeColor = () => {
-                    const bgColor = to.dataset.page;
-                    document.body.className = "";
-                    document.body.classList.add("body-" + bgColor);
-                }
-                changeColor();
-                removeClass('inactive-card');
-                done();
-            }
+            ease: "power4",
+            stagger: 0.25
         }, 0.0)
-        // .to(['.images-well img'], { 
-        //     duration: 0.5,
-        //     autoAlpha: 1,
-        //     ease: "power4",
-        // });
+        .fromTo('#skoot', { 
+            width: '0%'
+        },
+        {
+            duration: 2.5,
+            width: '100%',
+            ease: "power4"
+        }, 0.0);
     }
 
     out({ from, done }) {
-        // Tween.fromTo(from, 0.5,
-        //   { opacity: 1 },
-        //   {
-        //     opacity: 0,
-        //     onComplete: function() {
-        //         console.log("out")
-        //         done();
-        //     }
-        //   }
-        // );
-        // console.log('out')
         done();
     }
 }
